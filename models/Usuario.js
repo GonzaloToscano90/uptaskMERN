88d7400d46d6670626efaab3ee2c409eb5aaf42a
GonzaloToscano90 <zalo.jat@gmail.com>
2022-03-12 22:46:31 -0300

    creando el Modelo de Usuarios

---
+++ models/Usuario.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+//estructura de la base de datos
+const usuarioSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: "string",
+      required: true,
+      trim: true, //elimina los espacios del inicio y final
+    },
+    password: {
+      type: "string",
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: "string",
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    token: {
+      type: "string",
+    },
+    confirmado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+      timestamps: true, //crea dos columnas mas: una de creado y otra de actualizado
+  }
+);
+
+//Definir modelo:
+const Usuario = mongoose.model("Usuario",usuarioSchema);
+
+export default Usuario;
