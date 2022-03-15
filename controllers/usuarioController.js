35455d5587b81c197f16e4794948e4109ed23b73
GonzaloToscano90 <zalo.jat@gmail.com>
2022-03-14 22:23:31 -0300

    Prevenir usuarios duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,6 +2,15 @@ import Usuario from '../models/Usuario.js'
 
 // crear un nuevo usuario e insertarlo a la base de datos
 const registrar = async (req, res) => {
+    // Evitar registros duplicados
+    const { email } = req.body;
+    const existeUsuario = await Usuario.findOne({ email }); //find trae todos los usuarios registrados- findOne= encuentra el primero que coincida con {email}
+
+        if (existeUsuario) {
+            const error = new Error("Usuario ya Registrado")
+            return res.status(400).json({msg: error.message})
+        }
+
     try {
         const usuario = new Usuario(req.body)
         const usuarioAlmacenado = await usuario.save()
