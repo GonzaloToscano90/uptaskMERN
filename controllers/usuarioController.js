d16d772e65b47cc52f46bee2940bd1377da09d06
GonzaloToscano90 <zalo.jat@gmail.com>
2022-03-16 23:35:13 -0300

    Agregando validaciones para autenticacion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,27 +1,42 @@
-import Usuario from '../models/Usuario.js'
-import generarId from '../helpers/generarId.js'
+import Usuario from "../models/Usuario.js";
+import generarId from "../helpers/generarId.js";
 
 // crear un nuevo usuario e insertarlo a la base de datos
 const registrar = async (req, res) => {
-    // Evitar registros duplicados
-    const { email } = req.body;
-    const existeUsuario = await Usuario.findOne({ email }); //find trae todos los usuarios registrados- findOne= encuentra el primero que coincida con {email}
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email }); //find trae todos los usuarios registrados- findOne= encuentra el primero que coincida con {email}
 
-        if (existeUsuario) {
-            const error = new Error("Usuario ya Registrado")
-            return res.status(400).json({msg: error.message})
-        }
+  if (existeUsuario) {
+    const error = new Error("Usuario ya Registrado");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    try {
-        const usuario = new Usuario(req.body)
-        usuario.token = generarId();
-        const usuarioAlmacenado = await usuario.save()
-        res.json(usuarioAlmacenado);
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const usuario = new Usuario(req.body);
+    usuario.token = generarId();
+    const usuarioAlmacenado = await usuario.save();
+    res.json(usuarioAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+  // Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({email});  
+  console.log(usuario)
+  if(!usuario){
+      const error = new Error("El usuario no existe")
+      return res.status(404).json({msg: error.message})
+  }
+  // Comprobar si el usuario esta confirmado
+  if(!usuario.confirmado){
+    const error = new Error("Tu cuenta no ha sido confirmado")
+    return res.status(403).json({msg: error.message})
 }
+  // Comprobar su Password
+};
 
-export {registrar} 
+export { registrar, autenticar };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {registrar} from "../controllers/usuarioController.js"
+import {registrar, autenticar} from "../controllers/usuarioController.js"
 
 // Autenticación, Registro y Confirmación de Usuarios  
 router.post('/', registrar); // Crea un nuevo usuario
+router.post('/login', autenticar); 
 
 
 
