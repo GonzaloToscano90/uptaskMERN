07d70524571d813df2dc13c900b9a5bf9568cd06
GonzaloToscano90 <zalo.jat@gmail.com>
2022-04-07 21:48:38 -0300

    Creando Proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,5 +1,20 @@
+import Proyecto from "../models/Proyecto.js";
+
 const obtenerProyectos = async(req, res) => {};
-const nuevoProyecto = async(req, res) => {};
+const nuevoProyecto = async(req, res) => {
+    // console.log(req.body);
+    // console.log(req.usuario)
+    const proyecto = new Proyecto(req.body);
+    proyecto.creador = req.usuario._id;
+
+    try {
+        const proyectoAlmacenado = await proyecto.save();
+        res.json(proyectoAlmacenado);
+    } catch (error) {
+        console.log(error)
+    }
+
+};
 const obtenerProyecto = async(req, res) => {};
 const editarProyecto = async(req, res) => {};
 const eliminarProyecto = async(req, res) => {};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js"
+import proyectoRoutes from "./routes/proyectoRoutes.js"
 
 const app = express();
 
@@ -13,7 +14,7 @@ conectarDB();
 
 //Routing (agrupar las rutas para usuarios, proyectos, tareas) 
 app.use('/api/usuarios', usuarioRoutes) // .use soporta los verbos get,post,put,patch y deleted  
-app.use('/api/proyectos', usuarioRoutes)
+app.use('/api/proyectos', proyectoRoutes)
 
 const PORT = process.env.PORT || 4000;
 
